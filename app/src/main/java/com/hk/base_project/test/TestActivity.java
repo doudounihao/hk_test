d059e01071a481abb7240a9a0602d2a6bb317b33
doudounihao <18235148320@163.com>
2019-01-14 11:33:28 +0800

    Initial commit

---
+++ app/src/main/java/com/hk/base_project/test/TestActivity.java
@@ -0,0 +1,25 @@
+package com.hk.base_project.test;
+
+import com.hk.base_project.R;
+import com.jaydenxiao.common.base.BaseActivity;
+
+/**
+ * Created by hk on 2019/1/11.
+ */
+
+public class TestActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
